0deabc13c893533ac0418b2875f22ac0c37886b7
Maggie Chen <maggieyc2@gmail.com>
2024-10-14 01:41:11 +0200

    New fields (#1)
    * add new fields to account creation, and add account details modal
    
    * fix edit and details buttons overlap
    
    * add homepage
    
    * update workflow
    
    * update action versions
    
    * update .env to listen to mchen-be-dev

--- src/components/AppAccounts.vue
+++ src/components/AppAccounts.vue
@@ -6,7 +6,7 @@
           <h1>Accounts</h1>
           <hr />
           <br />
-          <!-- Allert Message -->
+          <!-- Alert Message -->
           <b-alert v-if="showMessage" variant="success" show>{{
             message
           }}</b-alert>
@@ -19,6 +19,7 @@
           >
             Create Account
           </button>
+
           <br /><br />
           <table class="table table-hover">
             <thead>
@@ -33,11 +34,11 @@
             </thead>
             <tbody>
               <tr v-for="account in accounts" :key="account.id">
-                <td>{{ account.name }}</td>
-                <td>{{ account.account_number }}</td>
-                <td>{{ account.balance }}</td>
-                <td>{{ account.currency }}</td>
-                <td>
+                <td @click="handleAccountClick(account)">{{ account.name }}</td>
+                <td @click="handleAccountClick(account)">{{ account.account_number }}</td>
+                <td @click="handleAccountClick(account)">{{ account.balance }}</td>
+                <td @click="handleAccountClick(account)">{{ account.currency }}</td>
+                <td @click="handleAccountClick(account)">
                   <span
                     v-if="account.status == 'Active'"
                     class="badge badge-success"
@@ -60,7 +61,7 @@
                     <button
                       type="button"
                       class="btn btn-danger btn-sm"
-                      @click="deleteAccount(account)"
+                      @click="confirmDeleteAccount(account)"
                     >
                       Delete
                     </button>
@@ -69,6 +70,13 @@
               </tr>
             </tbody>
           </table>
+
+          <router-link 
+            to="/" 
+            class="btn btn-primary btn-sm">
+            Back to Homepage
+          </router-link>
+
           <footer class="text-center">
             Copyright &copy; All Rights Reserved.
           </footer>
@@ -105,13 +113,67 @@
               id="form-currency-input"
               type="text"
               v-model="createAccountForm.currency"
+              :state="currencyState"
               placeholder="$ or €"
+              @input="currencyTouched = true"
+              required
+            >
+            </b-form-input>
+            <b-form-invalid-feedback v-if="currencyShowError && !currencyValid">
+                Currency must be "$" or "€".
+            </b-form-invalid-feedback>
+          </b-form-group>
+
+          <b-form-group
+            id="form-country-group"
+            label="Country:"
+            label-for="form-country-input"
+          >
+            <b-form-input
+              id="form-country-input"
+              type="text"
+              v-model="createAccountForm.country"
+              placeholder="Country"
               required
             >
             </b-form-input>
           </b-form-group>
 
-          <b-button type="submit" variant="outline-info">Submit</b-button>
+          <b-form-group
+            id="form-type-group"
+            label="Account Type:"
+            label-for="form-type-input"
+          >
+            <b-form-select 
+              v-model="createAccountForm.type" 
+              :options="createAccountForm.options"
+              required
+            >
+            </b-form-select>
+          </b-form-group>
+
+          <b-form-group
+            id="form-email-group"
+            label="Email:"
+            label-for="form-email-input"
+          >
+            <b-form-input
+              id="form-email-input"
+              type="text"
+              v-model="createAccountForm.email"
+              placeholder="Email"
+              :state="emailState"
+              @input="emailTouched = true"
+              required
+            >
+            </b-form-input>
+            <b-form-invalid-feedback v-if="emailShowError && !emailValid">
+                Please enter a valid email format.
+            </b-form-invalid-feedback>
+          </b-form-group>
+
+
+          <b-button type="submit" variant="outline-info"> Submit</b-button>
         </b-form>
       </b-modal>
       <!-- End of Modal for Create Account-->
@@ -138,10 +200,59 @@
             >
             </b-form-input>
           </b-form-group>
+
+          <b-form-group
+            id="form-edit-email-group"
+            label="Email:"
+            label-for="form-edit-email-input"
+          >
+            <b-form-input
+              id="form-edit-email-input"
+              type="text"
+              v-model="editAccountForm.email"
+              placeholder="Email"
+              required
+            >
+            </b-form-input>
+          </b-form-group>
           <b-button type="submit" variant="outline-info">Update</b-button>
         </b-form>
       </b-modal>
       <!-- End of Modal for Edit Account-->
+
+      <!-- Start of Modal for Delete Account-->
+      <b-modal
+        ref="deleteAccountModal"
+        id="delete-account-modal"
+        title="Delete account"
+        hide-backdrop
+        hide-footer
+      >
+        Are you sure you want to delete the account?
+      <button
+        type="button"
+        class="btn btn-danger btn-sm"
+        @click="deleteAccount(accountToDelete)"
+      >
+        Yes
+      </button>
+      </b-modal>
+      <!-- End of Modal for Delete Account-->
+
+      <!-- Start of Modal for Display Account-->
+      <b-modal ref="accountDetailsModal" id="account-details-modal" title="Account Details" hide-footer>
+      <div v-if="selectedAccount">
+        <p><strong>Account Name:</strong> {{ selectedAccount.name }}</p>
+        <p><strong>Account Number:</strong> {{ selectedAccount.account_number }}</p>
+        <p><strong>Account Balance:</strong> {{ selectedAccount.balance }}</p>
+        <p><strong>Currency:</strong> {{ selectedAccount.currency }}</p>
+        <p><strong>Status:</strong> {{ selectedAccount.status }}</p>
+        <p><strong>Country:</strong> {{ selectedAccount.country }}</p>
+        <p><strong>Email:</strong> {{ selectedAccount.email }}</p>
+        <p><strong>Account Type:</strong> {{ selectedAccount.type }}</p>
+      </div>
+    </b-modal>
+    <!-- End of Modal for Display Account-->
     </div>
   </div>
 </template>
@@ -153,19 +264,57 @@ export default {
   data() {
     return {
       accounts: [],
+      selectedAccount: null,
       createAccountForm: {
         name: "",
         currency: "",
+        country: "",
+        type: null,
+        options: [
+          { value: null, text: 'Please select an option' },
+          { value: 'Checking', text: 'Checking' },
+          { value: 'Savings', text: 'Savings' },
+        ],
+        email: "",
+        accountToDelete: null
       },
       editAccountForm: {
         id: "",
         name: "",
+        email: ""
       },
       showMessage: false,
       message: "",
+      currencyShowError: false,
+      currencyTouched: false,
+      emailError: false,
+      emailTouched: false,
     };
   },
+  computed: {
+    currencyValid() {
+      // Checks if the input is exactly "$" or "€"
+      return (this.createAccountForm.currency === "$" || this.createAccountForm.currency === "€");
+    },
+    currencyState() {
+      // Checks if the input is exactly "$" or "€"
+      return this.currencyTouched || this.currencyShowError ? this.currencyValid : null;
+    },
+    emailValid() {
+      // Email regex for basic validation
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(this.createAccountForm.email);
+    },
+    emailState() {
+      // Checks if the input is exactly "$" or "€"
+      return this.emailTouched || this.emailShowError ? this.emailValid : null;
+    },
+  },
   methods: {
+    handleAccountClick(account){
+      this.selectedAccount = account
+      this.$refs.accountDetailsModal.show()
+    },
     /***************************************************
      * RESTful requests
      ***************************************************/
@@ -257,28 +406,52 @@ export default {
     initForm() {
       this.createAccountForm.name = "";
       this.createAccountForm.currency = "";
+      this.createAccountForm.country = "";
+      this.createAccountForm.type = "";
+      this.createAccountForm.email = "";
       this.editAccountForm.id = "";
       this.editAccountForm.name = "";
+      this.editAccountForm.email = "";
     },
 
     // Handle submit event for create account
     onSubmit(e) {
       e.preventDefault(); //prevent default form submit form the browser
-      this.$refs.addAccountModal.hide(); //hide the modal when submitted
-      const payload = {
-        name: this.createAccountForm.name,
-        currency: this.createAccountForm.currency,
-      };
-      this.RESTcreateAccount(payload);
-      this.initForm();
+
+      this.emailShowError = true;
+      this.currencyShowError = true;
+
+      if (this.currencyValid){
+        this.currencyShowError = false;
+        this.currencyTouched = false;
+      }
+      if (this.emailValid){
+        this.emailShowError = false;
+        this.emailTouched = false;
+      }
+
+      if (this.currencyValid && this.emailValid){
+        this.$refs.addAccountModal.hide(); 
+        const payload = {
+          name: this.createAccountForm.name,
+          currency: this.createAccountForm.currency,
+          country: this.createAccountForm.country,
+          type: this.createAccountForm.type,
+          email: this.createAccountForm.email
+        };
+        this.RESTcreateAccount(payload);
+        this.initForm();
+      }
+
     },
 
     // Handle submit event for edit account
     onSubmitUpdate(e) {
-      e.preventDefault(); //prevent default form submit form the browser
-      this.$refs.editAccountModal.hide(); //hide the modal when submitted
+      e.preventDefault();
+      this.$refs.editAccountModal.hide();
       const payload = {
         name: this.editAccountForm.name,
+        email:this.editAccountForm.email
       };
       this.RESTupdateAccount(payload, this.editAccountForm.id);
       this.initForm();
@@ -286,12 +459,19 @@ export default {
 
     // Handle edit button
     editAccount(account) {
-      this.editAccountForm = account;
+      this.editAccountForm = { ...account };
+    },
+
+    // Show modal confirmation before deleting
+    confirmDeleteAccount(account) {
+      this.accountToDelete = account; 
+      this.$refs.deleteAccountModal.show();
     },
 
     // Handle Delete button
     deleteAccount(account) {
       this.RESTdeleteAccount(account.id);
+      this.$refs.deleteAccountModal.hide();
     },
   },
 
---
+++ src/components/Homepage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>IE Bank Corp Account Management System</h1>
+        <router-link to="/accounts" class="btn btn-primary btn-sm">
+          Go to Accounts
+        </router-link>
+
+    </div>
+</template>
+
+<script>
+export default
+    {
+        name: 'Homepage',
+    }
+</script>
